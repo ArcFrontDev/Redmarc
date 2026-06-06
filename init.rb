8ea60a8fcf6b5b21b3a63af1b85c950efa3e7169
ui-mike <mike@arcfront.tech>
2026-06-06 13:52:15 +0300

    fix: Rename plugin ID to :redmarc to match directory name

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require 'redmine'
 
-Redmine::Plugin.register :arcfront do
+Redmine::Plugin.register :redmarc do
   name 'Redmarc Frontend Plugin'
   author 'ArcFrontDev'
   description 'Modern React frontend for Redmine'
   version '0.0.1'
   url 'https://github.com/ArcFrontDev/Redmarc'
   author_url 'https://github.com/ArcFrontDev'
-  
+
   begin
     menu :top_menu, :redmarc, { :controller => 'redmarc', :action => 'index' }, :caption => 'Redmarc'
   rescue
